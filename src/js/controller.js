aa464264884d0c3898cc60852cf5be3b5b2f5d5d
matheusnsilva7 <silva.matheus6081@gmail.com>
2022-08-03 14:05:55 -0300

    project

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 class weeklyPlanning{ 
     _removeActivity = document.querySelector(".btn-remove");
     _addActivity = document.querySelector(".btn-add");
@@ -12,10 +15,10 @@ class weeklyPlanning{
     _localStorage = localStorage.getItem('name') ? JSON.parse(localStorage.getItem('name')) : [[],[],[],[],[]];
 
     constructor(){
-        this._removeActivity.addEventListener("click", this._removeActivitycontainerHandler.bind(this));
-        this._addActivity.addEventListener("click",this._addActivitycontainerHandler.bind(this));
+        this._removeActivity.addEventListener("click", this._removeActivityContainerHandler.bind(this));
+        this._addActivity.addEventListener("click",this._addActivityContainerHandler.bind(this));
         this._removeAllActivity.addEventListener("click",this._removeAllHandler.bind(this));
-        this._containerAddActivity.addEventListener("click",this._closecontainerAddACtivityHandler.bind(this));
+        this._containerAddActivity.addEventListener("click",this._closeContainerAddACtivityHandler.bind(this));
         this._form.addEventListener("submit",this._addHandlerActivity.bind(this));
         this._weekly.addEventListener("click",this._activityBtnHandler.bind(this));
         this._weekDays();
@@ -24,7 +27,7 @@ class weeklyPlanning{
     this._localStorage[day].push(obj);
     localStorage.setItem("name", JSON.stringify(this._localStorage));
     }
-    _cleanform(){
+    _cleanForm(){
         document.querySelector(".add__activity-box-Activity").value = "";
         document.querySelector(".add__activity-box-time").value = "";
     }
@@ -35,21 +38,21 @@ class weeklyPlanning{
         localStorage.setItem("name", []);
         this._localStorage = localStorage.getItem('name') ? JSON.parse(localStorage.getItem('name')) : [[],[],[],[],[]];
         this._weekDays();
-        this._removeActivitycontainerHandler();
+        this._removeActivityContainerHandler();
 
     }
     _filterDays(e){
         if(e.length <= 1) return;
         e.sort((a,b) => +a.time.replace(":" ,"") - +b.time.replace(":" ,"")); 
     }
-    _activitycheck(e){
-        e.target.parentElement.parentElement.firstElementChild.classList.toggle("active");
-        e.target.parentElement.parentElement.children[1].classList.toggle("not-active");
-        e.target.parentElement.parentElement.children[2].classList.toggle("text-line");
-        e.target.parentElement.parentElement.children[3].classList.toggle("text-line");
+    _activityCheck(e){
+        e.target.closest(".weekly__container-activity").firstElementChild.classList.toggle("active");
+        e.target.closest(".weekly__container-activity").children[1].classList.toggle("not-active");
+        e.target.closest(".weekly__container-activity").children[2].classList.toggle("text-line");
+        e.target.closest(".weekly__container-activity").children[3].classList.toggle("text-line");
     }
     _activityBtnHandler(e){
-        if(e.target.classList.contains("exit") || e.target.classList.contains("check") || e.target.classList.contains("ckecked")){
+        if(e.target.classList.contains("exit") || e.target.classList.contains("check") || e.target.classList.contains("checked")){
         const id= e.target.closest(".weekly__container-activity").id;
         const index = this._localStorage[+id[0]].findIndex(e => +e.id === +id);
         if(e.target.classList.contains("exit")){
@@ -61,12 +64,12 @@ class weeklyPlanning{
         if(e.target.classList.contains("check")){
             this._localStorage[id[0]][index].check = true;
             localStorage.setItem("name", JSON.stringify(this._localStorage));
-            this._activitycheck(e);
+            this._activityCheck(e);
         };
-        if( e.target.classList.contains("ckecked")){
+        if( e.target.classList.contains("checked")){
             this._localStorage[id[0]][index].check = false;
             localStorage.setItem("name", JSON.stringify(this._localStorage));
-            this._activitycheck(e);
+            this._activityCheck(e);
         }
         }
     }
@@ -77,29 +80,29 @@ class weeklyPlanning{
         };
         this._localStorage.forEach((e,i) => {
             this._filterDays(e);
-            this._desplayactivity(e , i);
+            this._desplayActivity(e , i);
         });
     }
-    _desplayactivity(day, index){
+    _desplayActivity(day, index){
         document.querySelector(`#day${index + 1}`).innerHTML = "";
         day.forEach(e => {
             const html = `
                 <div class="weekly__container-activity" id="${e.id}">
                     <div class="weekly-activity-checked ${e.check === true ? "active" :""}">
-                        <svg class="ckecked" xmlns="http://www.w3.org/2000/svg" height="20" width="20">
-                            <path d="m8.938 13 4.958-4.938L12.833 7l-3.895 3.875-1.771-1.75-1.063 1.063ZM4.5 17q-.625 0-1.062-.438Q3 16.125 3 15.5v-11q0-.625.438-1.062Q3.875 3 4.5 3h11q.625 0 1.062.438Q17 3.875 17 4.5v11q0 .625-.438 1.062Q16.125 17 15.5 17Zm0-1.5h11v-11h-11v11Zm0-11v11-11Z"/>
-                        </svg>
+                        <div class="checked-box checked">
+                            <svg class="checked" xmlns="http://www.w3.org/2000/svg" height="20" width="20">
+                                <path class="checked" d="m8.229 14.062-3.521-3.541L5.75 9.479l2.479 2.459 6.021-6L15.292 7Z"/>
+                            </svg>
+                        </div>
                     </div>
                     <div class="weekly-activity-box ${e.check === true ? "not-active" :""}">
-                        <svg class="check" xmlns="http://www.w3.org/2000/svg" height="20" width="20">
-                            <path d="M4.5 17q-.625 0-1.062-.438Q3 16.125 3 15.5v-11q0-.625.438-1.062Q3.875 3 4.5 3h11q.625 0 1.062.438Q17 3.875 17 4.5v11q0 .625-.438 1.062Q16.125 17 15.5 17Zm0-1.5h11v-11h-11v11Z"/>
-                        </svg>
+                        <div class="check"> </div>
                     </div>
                     <div class="weekly-activity-time ${e.check === true ? "text-line" :""}">${e.time}</div>
                     <div class="weekly-activity-name ${e.check === true ? "text-line" :""}">${e.activity}</div>
                     <div class="weekly-activity-exit">
                         <svg class="exit" xmlns="http://www.w3.org/2000/svg" height="20" width="20">
-                            <path d="M6.062 15 5 13.938 8.938 10 5 6.062 6.062 5 10 8.938 13.938 5 15 6.062 11.062 10 15 13.938 13.938 15 10 11.062Z"/>
+                            <path class="exit" d="M6.062 15 5 13.938 8.938 10 5 6.062 6.062 5 10 8.938 13.938 5 15 6.062 11.062 10 15 13.938 13.938 15 10 11.062Z"/>
                         </svg>
                     </div>
                 </div>
@@ -132,17 +135,17 @@ class weeklyPlanning{
         dataArr.push(["id", 0]);
         this._weekPush(dataArr);
         this._containerAddActivity.classList.remove("active");
-        this._cleanform();
+        this._cleanForm();
     }
-    _closecontainerAddACtivityHandler(e){
+    _closeContainerAddACtivityHandler(e){
         if(e.target.classList.contains("add__activity")){
             this._containerAddActivity.classList.remove("active");
         };
     }
-    _addActivitycontainerHandler(){
+    _addActivityContainerHandler(){
         this._containerAddActivity.classList.add("active");
     }
-    _removeActivitycontainerHandler(){
+    _removeActivityContainerHandler(){
         this._btnExit.forEach(e =>{
             e.classList.toggle("active");
         });
